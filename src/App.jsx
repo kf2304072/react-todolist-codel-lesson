682ea8489af9e3f1753cf4cfb478ec90985be272
kf2304072 <cometrue.6398@gmail.com>
2023-08-24 11:35:18 +0900

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
 
@@ -25,6 +25,12 @@ function App() {
   //新しいタイトルのstateを定義してinputと紐づけ
   const [newTitle, setNewTitle] = useState('')
 
+  //フィルターのstatusを定義
+  const [filter, setFilter] = useState('notStarted')
+
+  //絞り込まれたTodoリストのstate
+  const [filteredTodos, setFilteredTodos] = useState([])
+
   const handleAddFormChanges = (e) => {
     setTodoTitle(e.target.value)
   }
@@ -34,8 +40,7 @@ function App() {
   }
 
   const handleAddTodo = () => {
-    // 問題1. 新規todoにステータスプロパティを持たせよう
-    // ここまで
+    setTodos([...todos, { id: todoId, title: todoTitle, status: 'notStarted' }])
     setTodoId(todoId + 1)
     resetFormInput()
   }
@@ -69,6 +74,36 @@ function App() {
     handleCloseEditForm()
   }
 
+  const handleStatusChange = (targetTodo, e) => {
+    const newArray = todos.map((todo) =>
+      todo.id === targetTodo.id ? { ...todo, status: e.target.value } : todo
+    )
+    setTodos(newArray)
+  }
+
+  useEffect(() => {
+    const filteringTodos = () => {
+      switch (filter) {
+        case 'notStarted':
+          setFilteredTodos(todos.filter((todo) => todo.status === 'notStarted'))
+          break
+        // 問題1. 絞り込みの処理を書こう
+        case 'inProgress':
+          setFilteredTodos(todos.filter((todo) => todo.status === 'inProgress'))
+          break
+        case 'done':
+          setFilteredTodos(todos.filter((todo) => todo.status === 'done'))
+          break
+        // ここまで
+        default:
+          setFilteredTodos(todos)
+      }
+    }
+    // 問題2. filteringTodosを呼び出そう
+    filteringTodos()
+    // ここまで
+  }, [filter, todos])
+
   return (
     <>
       {isEditable ? (
@@ -91,19 +126,33 @@ function App() {
             onChange={handleAddFormChanges}
           />
           <button onClick={handleAddTodo}>作成</button>
+          <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+            <option value="all">すべて</option>
+            <option value="notStarted">未着手</option>
+            <option value="inProgress">作業中</option>
+            <option value="done">完了</option>
+          </select>
         </div>
       )}
+
       <ul>
-        {todos.map((todo) => (
+        {/* 問題3. 絞り込んだtodoを一覧に渡そう*/}
+        {filteredTodos.map((todo) => (
           <li key={todo.id}>
             <span>{todo.title}</span>
-            {/* 問題2. todoの隣にセレクトボックスを追加しよう */}
-
-            {/* ここまで */}
+            <select
+              value={todo.status}
+              onChange={(e) => handleStatusChange(todo, e)}
+            >
+              <option value="notStarted">未着手</option>
+              <option value="inProgress">作業中</option>
+              <option value="done">完了</option>
+            </select>
             <button onClick={() => handleOpenEditForm(todo)}>編集</button>
             <button onClick={() => handleDeleteTodo(todo)}>削除</button>
           </li>
         ))}
+        {/* ここまで */}
       </ul>
     </>
   )
